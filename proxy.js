28235a6478f697a2526802e5e933daebcfa3188d
Ed Summers <ehs@pobox.com>
2011-11-07 12:20:21 +0000

    manually set up proxy so I can log a bit better

--- proxy.js
+++ proxy.js
@@ -1,13 +1,19 @@
 var http = require('http'),
     httpProxy = require('http-proxy');
 
-var options = {
-    router: {
-        'wikistream.inkdroid.org': '127.0.0.1:3000',
-        'inkdroid.org': '127.0.0.1:9000',
-        'linkypedia.inkdroid.org': '127.0.0.1:9000',
-        'linkypedia.info': '127.0.0.1:9000',
-    }
-}
+var proxyServer = httpProxy.createServer(function(req, res, proxy) {
+  console.log(JSON.stringify({"url": req.url, headers: req.headers}));
 
-var proxyServer = httpProxy.createServer(options).listen(8000);
+  host = req.headers['host'];
+  if (host == "wikistream.inkdroid.org") {
+    // wikistream is served up by a node app on port 3000
+    port = 3000;
+  } else {
+    // everything else goese to Apache on port 9000
+    port = 9000;
+  }
+
+  proxy.proxyRequest(req, res, {host: 'localhost', port: port});
+});
+
+proxyServer.listen(8000);
